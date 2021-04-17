8775754d40c0ec5147ac2c6069f90cd13cc2f0c8
somphon <mankky082@hotmail.com>
2021-04-17 17:24:14 +0700

    COR_ALLOWED_ORIGINS

--- recommendationSystem/settings.py
+++ recommendationSystem/settings.py
@@ -34,6 +34,9 @@ CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
     'http://localhost:5000',
     'http://localhost:8000',
+    'http://127.0.0.1:8000',
+    'http://127.0.0.1:3000',
+    'http://127.0.0.1:8000',
     'https://student-recommend-api.herokuapp.com/'
 ]
 
